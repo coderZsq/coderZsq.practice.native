b6b066d8e8759aeb9a772fa2a5890cab6ac15ecd
coderZsq <a13701777868@yahoo.com>
2018-02-26 14:46:02 +0800

    addd

---
+++ StudyNotes/Algorithm/Search.swift
@@ -0,0 +1,41 @@
+//
+//  Search.swift
+//  Algorithm
+//
+//  Created by 朱双泉 on 26/02/2018.
+//  Copyright © 2018 Castie!. All rights reserved.
+//
+
+import Foundation
+
+@discardableResult func binSearch(list: [Int], find: Int) -> Int {
+
+    var low = 0, high = list.count - 1
+    while low <= high {
+        let mid = (low + high) / 2
+        if find == list[mid] {return mid}
+        else if (find > list[mid]) {low = mid + 1}
+        else {high = mid - 1}
+    }
+    return -1;
+}
+
+@discardableResult func recursiveBinSearch(list: [Int], find: Int) -> Int {
+    
+    func search(list: [Int], low: Int, high: Int, find: Int) -> Int {
+        if low <= high {
+            let mid = (low + high) / 2
+            if find == list[mid] {return mid}
+            else if (find > list[mid]) {
+                return search(list: list, low: mid+1, high: high, find: find)
+            }
+            else {
+                return search(list: list, low: low, high: mid-1, find: find)
+            }
+        }
+        return -1;
+    }
+    return search(list: list, low: 0, high: list.count - 1, find: find)
+}
+
+
---
+++ StudyNotes/Algorithm/main.swift
@@ -0,0 +1,87 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 朱双泉 on 26/02/2018.
+//  Copyright © 2018 Castie!. All rights reserved.
+//
+
+import Foundation
+
+public func scope(of description: String, execute: Bool, action: () -> ()) {
+    guard execute else { return }
+    print("--- scope of:", description, "---")
+    action()
+}
+
+public func timing(_ action: () -> ()) {
+    let time = NSDate().timeIntervalSince1970
+    action()
+    print("timing: \(NSDate().timeIntervalSince1970 - time)")
+}
+
+public func randomList(_ num: UInt32) -> [Int] {
+    var list = [Int]()
+    for _ in 0..<num {
+        list.append(Int(arc4random() % num) + 1)
+    }
+    return list;
+}
+
+scope(of: "sort", execute: false) {
+    
+    scope(of: "selectsort", execute: true, action: {
+        var list = randomList(10000)
+        timing {selectSort(list: &list)}
+//        print(list)
+    })
+
+    scope(of: "opt_selectsort", execute: true, action: {
+        var list = randomList(10000)
+        timing {optimizationSelectSort(list: &list)}
+//        print(list)
+    })
+
+    scope(of: "popsort", execute: true, action: {
+        var list = randomList(10000)
+        timing {popSort(list: &list)}
+//        print(list)
+    })
+
+    scope(of: "opt_popsort", execute: true, action: {
+        var list = randomList(10000)
+        timing {optimizationPopSort(list: &list)}
+//        print(list)
+    })
+
+    scope(of: "quicksort", execute: true, action: {
+        var list = randomList(10000)
+        timing {quickSort(list: &list)}
+//        print(list)
+    })
+}
+
+scope(of: "search", execute: true) {
+    
+    scope(of: "binsearch", execute: true, action: {
+        var list = randomList(10000)
+        var index = 0
+        timing {
+            quickSort(list: &list)
+            //        print(list)
+            index = binSearch(list: list, find: 6)
+        }
+        print("index: \(index)")
+    })
+    
+    scope(of: "rec_binsearch", execute: true, action: {
+        var list = randomList(10000)
+        var index = 0
+        timing {
+            quickSort(list: &list)
+            //        print(list)
+            index = recursiveBinSearch(list: list, find: 6)
+        }
+        print("index: \(index)")
+    })
+}
