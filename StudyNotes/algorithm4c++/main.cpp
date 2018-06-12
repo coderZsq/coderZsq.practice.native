75b1d24f622905146429d5899fad18fb5eb46842
coderZsq <a13701777868@yahoo.com>
2018-06-12 16:11:52 +0800

    add

--- StudyNotes/algorithm4c++/main.cpp
+++ StudyNotes/algorithm4c++/main.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <queue>
 
 #include "SortTestHelper.hpp"
 #include "SelectionSort.hpp"
@@ -22,298 +21,239 @@ using namespace std;
 
 void sortTest();
 
-template <typename Key, typename Value>
-class BST {
+class UnionFind5 {
 private:
-    struct Node {
-        Key key;
-        Value value;
-        Node * left;
-        Node * right;
-        
-        Node(Key key, Value value) {
-            this->key = key;
-            this->value = value;
-            this->left = this->right = NULL;
-        }
-        
-        Node(Node * node) {
-            this->key = node->key;
-            this->value = node->value;
-            this->left = node->left;
-            this->right = node->right;
-        }
-    };
-    
-    Node * root;
+    int * parent;
+    int * rank;//rank[i]表示以i为根的集合所表示的树的层数
     int count;
     
 public:
-    BST() {
-        root = NULL;
-        count = 0;
-    }
-    ~BST() {
-        destory(root);
-    }
-    
-    int size() {
-        return count;
-    }
-    
-    bool isEmpty() {
-        return count == 0;
-    }
-    
-    void insert(Key key, Value value) {
-        root = insert(root, key, value);
-    }
-    
-    Value * search(Key key) {
-        return search(root, key);
-    }
-    //前序遍历
-    void preOrder() {
-        preOrder(root);
-    }
-    //中序遍历
-    void inOrder() {
-        inOrder(root);
-    }
-    //后序遍历
-    void postOrder() {
-        postOrder(root);
-    }
-    //层序遍历
-    void levelOrder() {
-        queue<Node *> q;
-        q.push(root);
-        while (!q.empty()) {
-            Node * node = q.front();
-            q.pop();
-            cout<<node->key<<endl;
-            if (node->left) {
-                q.push(node->left);
-            }
-            if (node->right) {
-                q.push(node->right);
-            }
-        }
-    }
-    //寻找最小的键值
-    Key minimum() {
-        assert(count != 0);
-        Node * minNode = minimum(root);
-        return minNode->key;
-    }
-    //寻找最大的键值
-    Key maximum() {
-        assert(count != 0);
-        Node * maxNode = maximum(root);
-        return maxNode->key;
-    }
-    //从二叉树中删除最小值所在节点
-    void removeMin() {
-        if (root) {
-            root = removeMin(root);
-        }
-    }
-    //从二叉树中删除最大值所在节点
-    void removeMax() {
-        if (root) {
-            root = removeMax(root);
-        }
-    }
-    //从二叉树中删除键值为key的节点
-    void remove(Key key) {
-        root = remove(root, key);
-    }
-private:
-    //向以node为根的二叉搜索树中, 插入节点(key, value)
-    //返回插入新节点后的二叉搜索树的根
-    Node * insert(Node * node, Key key, Value value) {
-        if (node == NULL) {
-            count++;
-            return new Node(key, value);
-        }
-        if (key == node->key) {
-            node->value = value;
-        } else if (key < node->key) {
-            node->left = insert(node->left, key, value);
-        } else {//key > node->key
-            node->right = insert(node->right, key, value);
-        }
-        return node;
-    }
-    
-    //查看以node为根的二叉搜索树中是否包含键值为key的节点
-    bool contain(Node * node, Key key) {
-        if (node == NULL) {
-            return false;
-        }
-        if (key == node->key) {
-            return true;
-        } else if (key < node->key) {
-            return contain(node->left, key);
-        } else {//key > node->key
-            return contain(node->right, key);
+    UnionFind5(int count) {
+        parent = new int[count];
+        rank = new int[count];
+        this->count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            rank[i] = 1;
         }
     }
     
-    //在以node为根的二叉搜索树中查找key所对应的value
-    Value * search(Node * node, Key key) {
-        if (node == NULL) {
-            return NULL;
-        }
-        if (key == node->key) {
-            return &(node->value);
-        } else if (key < node->key) {
-            return search(node->left, key);
-        } else {//key > node->key
-            return search(node->right, key);
-        }
+    ~UnionFind5() {
+        delete []parent;
+        delete []rank;
     }
     
-    //对以node为根的二叉搜索树进行前序遍历
-    void preOrder(Node * node) {
-        if (node != NULL) {
-            cout<<node->key<<endl;
-            preOrder(node->left);
-            preOrder(node->right);
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
         }
+        return p;
+//        if (p != parent[p]) {
+//            parent[p] = find(parent[p]);
+//        }
+//        return parent[p];
     }
     
-    //对以node为根的二叉搜索树进行中序遍历
-    void inOrder(Node * node) {
-        if (node != NULL) {
-            preOrder(node->left);
-            cout<<node->key<<endl;
-            preOrder(node->right);
-        }
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
     }
-    
-    //对以node为根的二叉搜索树进行后序遍历
-    void postOrder(Node * node) {
-        if (node != NULL) {
-            preOrder(node->left);
-            preOrder(node->right);
-            cout<<node->key<<endl;
+    void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
         }
-    }
-    
-    void destory(Node * node) {
-        if (node != NULL) {
-            destory(node->left);
-            destory(node->right);
-            delete node;
-            count--;
-        }
-    }
-    
-    //在以node为根的二叉搜索树中, 返回最小键值的节点
-    Node * minimum(Node * node) {
-        if (node->left == NULL) {
-            return node;
-        }
-        return minimum(node->left);
-    }
-    
-    //在以node为根的二叉搜索树中, 返回最大键值的节点
-    Node * maximum(Node * node) {
-        if (node->right == NULL) {
-            return node;
-        }
-        return maximum(node->left);
-    }
-    
-    //删除掉以node为根的二分搜索树中的最小节点
-    //返回删除节点后新的二分搜索树的根
-    Node * removeMin(Node * node) {
-        if (node->left == NULL) {
-            Node * rightNode = node->right;
-            delete node;
-            count--;
-            return rightNode;
-        }
-        node->left = removeMin(node->left);
-        return node;
-    }
-    
-    //删除掉以node为根的二分搜索树中的最大节点
-    //返回删除节点后新的二分搜索树的根
-    Node * removeMax(Node * node) {
-        if (node->right == NULL) {
-            Node * leftNode = node->left;
-            delete node;
-            count--;
-            return leftNode;
-        }
-        node->right = removeMax(node->right);
-        return node;
-    }
-    
-    //删除掉以node为根的二分搜索树中键值为key的节点
-    //返回删除节点后新的二分搜索树的根
-    Node * remove(Node * node, Key key) {
-        if (node == NULL) {
-            return NULL;
-        }
-        if (key < node->key) {
-            node->left = remove(node->left, key);
-            return node;
-        } else if (key > node->key) {
-            node->right = remove(node->right, key);
-            return node;
-        } else {//key == node->key
-            if (node->left == NULL) {
-                Node * rightNode = node->right;
-                delete node;
-                count--;
-                return rightNode;
-            }
-            if (node->right == NULL) {
-                Node * leftNode = node->left;
-                delete node;
-                count--;
-                return leftNode;
-            }
-            //node->left != NULL && node->right != NULL
-            Node * successor = new Node(minimum(node->right));
-            count++;
-            
-            successor->right = removeMin(node->right);
-            successor->left = node->left;
-            
-            delete node;
-            count--;
-            
-            return successor;
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[qRoot] < rank[pRoot]) {
+            parent[qRoot] = pRoot;
+        } else {//rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
         }
     }
 };
 
-//二分查找从法, 在有序数组arr中, 查找target
-//如果找到target, 返回相应的索引index
-//如果没有找到target, 返回-1
-template <typename T>
-int binarySearch(T arr[], int n, T target) {
+class UnionFind4 {
+private:
+    int * parent;
+    int * rank;//rank[i]表示以i为根的集合所表示的树的层数
+    int count;
     
-    //在arr[l...r]之中查找target
-    int l = 0, r = n - 1;
-    while (l <= r) {
-//        int mid = (l + r) / 2;
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == target) return mid;
-        if (target < arr[mid]) {
-            //在arr[l...mid - 1]之中查找target
-            r = mid - 1;
-        } else {//target > arr[mid]
-            //在arr[mid + 1...r]之中查找target
-            l = mid + 1;
+public:
+    UnionFind4(int count) {
+        parent = new int[count];
+        rank = new int[count];
+        this->count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            rank[i] = 1;
         }
     }
-    return -1;
-}
+    
+    ~UnionFind4() {
+        delete []parent;
+        delete []rank;
+    }
+    
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+    
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+    void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[qRoot] < rank[pRoot]) {
+            parent[qRoot] = pRoot;
+        } else {//rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
+        }
+    }
+};
+
+class UnionFind3 {
+private:
+    int * parent;
+    int * sz;//sz[i]表示以i为根的集合中元素个数
+    int count;
+    
+public:
+    UnionFind3(int count) {
+        parent = new int[count];
+        sz = new int[count];
+        this->count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind3() {
+        delete []parent;
+        delete []sz;
+    }
+    
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+    
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+    void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        if (sz[pRoot] < sz[qRoot]) {
+            parent[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        } else {
+            parent[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+    }
+};
+
+class UnionFind2 {
+private:
+    int * parent;
+    int count;
+    
+public:
+    UnionFind2(int count) {
+        parent = new int[count];
+        this->count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    ~UnionFind2() {
+        delete []parent;
+    }
+    
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+    
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+    void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        parent[pRoot] = qRoot;
+    }
+};
+
+class UnionFind {
+private:
+    int * id;
+    int count;
+    
+public:
+    UnionFind(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+    }
+    
+    ~UnionFind() {
+        delete []id;
+    }
+    
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        return id[p];
+    }
+    
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+    
+    void unionElements(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+        if (pID == qID) {
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            if (id[i] == pID) {
+                id[q] = qID;
+            }
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
     
