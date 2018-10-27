de703ab7d3ae28ef910487f13fe929c071c56ac6
coderZsq <a13701777868@yahoo.com>
2018-10-27 11:26:58 +0800

    add

---
+++ StudyNotes/ReverseEngineering/MFCApplication/resource.h
@@ -0,0 +1,25 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFCApplication.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDM_GitHub                      1000
+#define IDC_GITHUB                      1000
+#define IDC_SUN                         1001
+#define IDC_CHECK2                      1002
+#define IDC_KILL                        1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
