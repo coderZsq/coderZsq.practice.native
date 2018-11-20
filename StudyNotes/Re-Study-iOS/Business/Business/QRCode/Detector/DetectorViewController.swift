22c4cb68d492704f1a0c0abeb575a3123ada5274
coderZsq <a13701777868@yahoo.com>
2018-11-20 17:08:28 +0800

    add

--- StudyNotes/Re-Study-iOS/Business/Business/QRCode/Detector/DetectorViewController.swift
+++ StudyNotes/Re-Study-iOS/Business/Business/QRCode/Detector/DetectorViewController.swift
@@ -10,8 +10,48 @@ import UIKit
 
 class DetectorViewController: UIViewController {
 
+    @IBOutlet weak var sourceImageView: UIImageView!
+    
+    @IBOutlet weak var resultImageView: UIImageView!
+    
+    @IBAction func detectorQRCode(_ sender: UIButton) {
+        let context = CIContext()
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        if
+            var image = sourceImageView.image,
+            let ciImage = CIImage(image: image),
+            let features = detector?.features(in: ciImage) {
+            for feature in features {
+                if let tempFeature = feature as? CIQRCodeFeature {
+                    print(tempFeature.bounds)
+                    print(tempFeature.messageString ?? "")
+                    image = drawQRCodeFrame(feature: tempFeature, toImage: image)!
+                    resultImageView.image = image
+                }
+            }
+        }
+    }
+    
+    func drawQRCodeFrame(feature: CIQRCodeFeature, toImage: UIImage) -> UIImage? {
+        let bounds = feature.bounds
+        let size = toImage.size
+        UIGraphicsBeginImageContext(size)
+        toImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let context = UIGraphicsGetCurrentContext()
+        context?.scaleBy(x: 1, y: -1)
+        context?.translateBy(x: 0, y: -size.height)
+        let path = UIBezierPath(rect: bounds)
+        path.lineWidth = 10
+        UIColor.red.set()
+        path.stroke()
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Detector"
     }
 }
+
--- StudyNotes/Re-Study-iOS/Business/Business/QRCode/Scanning/ScanningViewController.swift
+++ StudyNotes/Re-Study-iOS/Business/Business/QRCode/Scanning/ScanningViewController.swift
@@ -7,11 +7,88 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ScanningViewController: UIViewController {
-
+    
+    @IBOutlet weak var scanFrameBackView: UIImageView!
+    
+    @IBOutlet weak var toBottom: NSLayoutConstraint!
+    
+    var session = AVCaptureSession()
+    
+    lazy var input: AVCaptureDeviceInput? = {
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                return try AVCaptureDeviceInput(device: device)
+            } catch {
+                print(error)
+                return nil
+            }
+        }
+        return nil
+    }()
+    
+    lazy var output: AVCaptureMetadataOutput = {
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        return output
+    }()
+    
+    lazy var preLayer: AVCaptureVideoPreviewLayer = {
+        let preLayer = AVCaptureVideoPreviewLayer(session: session)
+        view.layer.insertSublayer(preLayer, at: 0)
+        return preLayer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Scanning"
+        setInit()
+        view.backgroundColor = .red
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        beginAnimation()
+        beginScan()
+    }
+}
+
+extension ScanningViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        print("扫描到结果")
+    }
+}
+
+extension ScanningViewController {
+    
+    func beginScan() {
+        if let input = input {
+            if session.canAddInput(input) && session.canAddOutput(output) {
+                session.addInput(input)
+                session.addOutput(output)
+                //            output.availableMetadataObjectTypes
+                output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            }
+        }
+        session.startRunning()
+        preLayer.frame = CGRect(x: 0, y: -600, width: view.width, height: 1000)
+    }
+    
+    func setInit() {
+        if let image = UIImage(named: "qrcode_border") {
+            scanFrameBackView.image = image.stretchableImage(withLeftCapWidth: Int(image.size.width * 0.5), topCapHeight: Int(image.size.height * 0.5))
+        }
+    }
+    
+    func beginAnimation() {
+        toBottom.constant = scanFrameBackView.frame.size.height
+        view.layoutIfNeeded()
+        toBottom.constant = -scanFrameBackView.frame.size.height
+        UIView.animate(withDuration: 1.5) {
+            UIView.setAnimationRepeatCount(Float.infinity)
+            self.view.layoutIfNeeded()
+        }
     }
 }
