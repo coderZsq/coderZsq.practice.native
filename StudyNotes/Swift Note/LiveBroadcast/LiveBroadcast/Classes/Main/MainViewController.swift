39c7ca0c38701d3275020481c363ac1952bc3f50
coderZsq <a13701777868@yahoo.com>
2018-12-07 17:37:33 +0800

    add

---
+++ StudyNotes/Swift Note/LiveBroadcast/LiveBroadcast/Classes/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  LiveBroadcast
+//
+//  Created by 朱双泉 on 2018/12/7.
+//  Copyright © 2018 Castie!. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
