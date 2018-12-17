344883e1a9b744b775048671d799982416a846eb
coderZsq <a13701777868@yahoo.com>
2018-12-17 15:30:32 +0800

    add

---
+++ StudyNotes/Swift Note/LiveBroadcast/Client/LiveBroadcast/Classes/Main/Extension/UIImageView+Extension.swift
@@ -0,0 +1,66 @@
+//
+//  UIImageView+Extension.swift
+//  LiveBroadcast
+//
+//  Created by 朱双泉 on 2018/12/17.
+//  Copyright © 2018 Castie!. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+
+extension UIImageView {
+    
+    func blurImageNamed(_ name: String) {
+        guard let sourceImage = UIImage(named: name) else { return }
+        let picProcess = GPUImagePicture(image: sourceImage)
+        let blurFilter = GPUImageGaussianBlurFilter()
+        blurFilter.texelSpacingMultiplier = 5
+        blurFilter.blurRadiusInPixels = 5
+        picProcess?.addTarget(blurFilter)
+        blurFilter.useNextFrameForImageCapture()
+        picProcess?.processImage()
+        let newImage = blurFilter.imageFromCurrentFramebuffer()
+        self.image = newImage
+    }
+    
+    func processImageNamed(_ name: String, filter: GPUImageFilter) {
+        let picProcess = GPUImagePicture(image: image)
+        picProcess?.addTarget(filter)
+        filter.useNextFrameForImageCapture()
+        picProcess?.processImage()
+        self.image = filter.imageFromCurrentFramebuffer()
+    }
+    
+    func gifImageNamed(_ name: String) {
+        guard let path = Bundle.main.path(forResource: name, ofType: "gif") else {
+            return
+        }
+        guard let data = NSData(contentsOfFile: path) else { return }
+        guard let imageSource = CGImageSourceCreateWithData(data, nil) else {
+            return
+        }
+        let imageCount = CGImageSourceGetCount(imageSource)
+        var images = [UIImage]()
+        var totalDuration: TimeInterval = 0
+        for i in 0..<imageCount {
+            guard let cgImage = CGImageSourceCreateImageAtIndex(imageSource, i, nil) else {
+                continue
+            }
+            let image = UIImage(cgImage: cgImage)
+            if i == 0 {
+                self.image = image
+            }
+            images.append(image)
+            guard let properties: NSDictionary = CGImageSourceCopyPropertiesAtIndex(imageSource, i, nil) else { continue }
+            guard let gifDict = properties[kCGImagePropertyGIFDictionary] as? NSDictionary else { continue }
+            guard let frameDuration = gifDict[kCGImagePropertyGIFDelayTime] as? NSNumber else { continue }
+            totalDuration += frameDuration.doubleValue
+        }
+        self.animationImages = images
+        self.animationDuration = totalDuration
+        self.animationRepeatCount = 0
+        self.startAnimating()
+    }
+    
+}
--- StudyNotes/Swift Note/LiveBroadcast/Client/LiveBroadcast/Classes/Rank/RankViewController.swift
+++ StudyNotes/Swift Note/LiveBroadcast/Client/LiveBroadcast/Classes/Rank/RankViewController.swift
@@ -7,15 +7,165 @@
 //
 
 import UIKit
-import ImageIO
+import AVFoundation
+import GPUImage
 
 class RankViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    fileprivate lazy var camera = GPUImageStillCamera(sessionPreset: AVCaptureSession.Preset.high.rawValue, cameraPosition: .front)
+    fileprivate lazy var camera2 = GPUImageVideoCamera(sessionPreset: AVCaptureSession.Preset.hd1280x720.rawValue, cameraPosition: .back)
+    fileprivate lazy var filter = GPUImageBrightnessFilter()
+    
+    fileprivate lazy var session = AVCaptureSession()
+    fileprivate var videoOutput: AVCaptureVideoDataOutput?
+    fileprivate var previewLayer: AVCaptureVideoPreviewLayer?
+    fileprivate var videoInput: AVCaptureDeviceInput?
+    fileprivate var movieOutput: AVCaptureMovieFileOutput?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupVideoInputOutput()
+        setupAudioInputOutput()
+        setupPreviewLayer()
         
+//        filter.brightness = 0.7
+//        camera?.addTarget(filter)
+        
+        camera2?.outputImageOrientation = .portrait
+        camera2?.addTarget(filter)
+        camera2?.delegate = self
+        let showView = GPUImageView(frame: view.bounds)
+        view.insertSubview(showView, at: 0)
+        filter.addTarget(showView)
+        camera2?.startCapture()
+    }
+
+}
+
+extension RankViewController: GPUImageVideoCameraDelegate {
+    
+    func willOutputSampleBuffer(_ sampleBuffer: CMSampleBuffer!) {
+        print(#function, #line)
+    }
+    
+}
+
+extension RankViewController {
+    
+    @IBAction func startCapturing(_ sender: UIButton) {
+        
+        camera?.capturePhotoAsImageProcessedUp(toFilter: filter, withCompletionHandler: { (image, error) in
+            DispatchQueue.main.async {
+                UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+                self.imageView.image = image
+                self.camera?.stopCapture()
+            }
+        })
+        
+        session.startRunning()
+        setupMovieFileOutput()
+    }
+    
+    @IBAction func stopCapturing(_ sender: UIButton) {
+        movieOutput?.stopRecording()
+        session.stopRunning()
+        previewLayer?.removeFromSuperlayer()
+    }
+    
+    @IBAction func rotateCamera(_ sender: UIButton) {
+        guard let videoInput = videoInput else { return }
+        let oritation: AVCaptureDevice.Position = videoInput.device.position == .front ? .back : .front
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTelephotoCamera, .builtInTrueDepthCamera, .builtInWideAngleCamera], mediaType: AVMediaType.video, position: oritation)
+        guard let device = discoverySession.devices.filter({ $0.position == oritation }).first else { return }
+        guard let newInput = try? AVCaptureDeviceInput(device: device) else { return }
+        session.beginConfiguration()
+        session.removeInput(videoInput)
+        if session.canAddInput(newInput) {
+            session.addInput(newInput)
+        }
+        session.commitConfiguration()
+        self.videoInput = newInput
     }
 }
 
+extension RankViewController {
+    
+    fileprivate func setupVideoInputOutput() {
+        guard let device = AVCaptureDevice.default(.builtInTrueDepthCamera, for: AVMediaType.video, position: .front) else { return }
+//        let devices = AVCaptureDevice.devices()
+//        guard let device = devices.filter({ $0.position == .front }).first else { return }
+        guard let input = try? AVCaptureDeviceInput(device: device) else { return }
+        videoInput = input
+        let output = AVCaptureVideoDataOutput()
+        videoOutput = output
+        let queue = DispatchQueue.global()
+        output.setSampleBufferDelegate(self, queue: queue)
+        addInputOutputToSession(input, output)
+    }
+    
+    fileprivate func setupAudioInputOutput() {
+        guard let device = AVCaptureDevice.default(for: .audio) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: device) else { return }
+        let output = AVCaptureAudioDataOutput()
+        let queue = DispatchQueue.global()
+        output.setSampleBufferDelegate(self, queue: queue)
+        addInputOutputToSession(input, output)
+    }
+    
+    private func addInputOutputToSession(_ input: AVCaptureInput, _ output: AVCaptureOutput) {
+        session.beginConfiguration()
+        if session.canAddInput(input) {
+            session.addInput(input)
+        }
+        if session.canAddOutput(output) {
+            session.addOutput(output)
+        }
+        session.commitConfiguration()
+    }
+    
+    fileprivate func setupPreviewLayer() {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.bounds
+        self.previewLayer = previewLayer
+        view.layer.insertSublayer(previewLayer, at: 0)
+    }
+    
+    fileprivate func setupMovieFileOutput() {
+        let fileOutput = AVCaptureMovieFileOutput()
+        movieOutput = fileOutput
+        let connection = fileOutput.connection(with: .video)
+        connection?.automaticallyAdjustsVideoMirroring = true
+        if session.canAddOutput(fileOutput) {
+            session.addOutput(fileOutput)
+        }
+        let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! + "/abc.mp4"
+        let fileURL = URL(fileURLWithPath: filePath)
+        fileOutput.startRecording(to: fileURL, recordingDelegate: self)
+    }
+    
+}
 
+extension RankViewController: AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if videoOutput?.connection(with: .video) == connection {
+            print("采集视频数据")
+        } else {
+            print("采集音频数据")
+        }
+    }
+    
+}
+
+extension RankViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        print("开始写入文件")
+    }
+        
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        print("完成写入文件")
+    }
+    
+}
