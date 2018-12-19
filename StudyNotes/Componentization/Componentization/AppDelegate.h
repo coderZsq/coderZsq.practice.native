880d9950a3445fa256209cd7500a2472bafed4fc
coderZsq <a13701777868@yahoo.com>
2018-12-19 15:25:28 +0800

    add

---
+++ StudyNotes/Componentization/Componentization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Componentization
+//
+//  Created by 朱双泉 on 2018/12/19.
+//  Copyright © 2018 Castie!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
