fc1c6e2522963fba282a86704c31654dc7fabbb1
coderZsq <a13701777868@yahoo.com>
2019-03-13 01:03:29 +0800

    add

--- StudyNotes/ReactiveCocoa/ReactiveCocoa/Server/model/User.js
+++
@@ -1,9 +0,0 @@
-function User(age, name) {
-    this.age = age;
-    this.name = name;
-    this.eat = function() {
-        console.log('吃东西');
-    };
-}
-
-module.exports = User;
--- StudyNotes/ReactiveCocoa/ReactiveCocoa/Server/utils/tool.js
+++
@@ -1,8 +0,0 @@
-var age = 20;
-
-function log() {
-    console.log('打印');
-}
-
-exports.age = age;
-exports.log = log;
