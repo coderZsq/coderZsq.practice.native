10aed5ea37d0a38df73f2e9b43997e74d2e08a6a
coderZsq <a13701777868@yahoo.com>
2019-06-18 15:27:51 +0800

    add

---
+++ StudyNotes/Lang Collection/Swift/Grammar.playground/Pages/02-数据类型.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+let age1: Int
+age1 = 10
+
+var num = 10
+num += 20
+num += 30
+let age2 = num
+
+func getAge() -> Int {
+    return 10
+}
+
+let age3 = getAge()
+print(age3)
+
+print(UInt.max)
+
+let error = (404, "Not Found")
+error.0
+error.1
+
