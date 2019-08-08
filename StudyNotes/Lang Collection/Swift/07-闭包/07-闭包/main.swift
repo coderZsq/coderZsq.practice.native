8b662d1efbc270865f2f7191b085e5f7acd5fb93
coderZsq <a13701777868@yahoo.com>
2019-08-08 15:24:46 +0800

    add

--- StudyNotes/Lang Collection/Swift/07-闭包/07-闭包/main.swift
+++ StudyNotes/Lang Collection/Swift/07-闭包/07-闭包/main.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Castie!. All rights reserved.
 //
 
-// MARK: - 闭包表达式
+// MARK: - 闭包表达式 (Closure Expression)
 
 do {
     func sum(_ v1: Int, _ v2: Int) -> Int { v1 + v2 }
@@ -114,6 +114,33 @@ do {
 // MARK: - 闭包 (Closure)
 
 do {
+    /*
+     07-闭包`getFn #1 () in :
+     0x100004e30 <+0>:  pushq  %rbp
+     0x100004e31 <+1>:  movq   %rsp, %rbp
+     0x100004e34 <+4>:  subq   $0x20, %rsp
+     0x100004e38 <+8>:  leaq   0x32c1(%rip), %rdi
+     0x100004e3f <+15>: movl   $0x18, %esi // 分配空间
+     0x100004e44 <+20>: movl   $0x7, %edx
+     0x100004e49 <+25>: callq  0x1000078e6               ; symbol stub for: swift_allocObject
+     0x100004e4e <+30>: movq   %rax, %rdx
+     0x100004e51 <+33>: addq   $0x10, %rdx
+     0x100004e55 <+37>: movq   %rdx, %rsi
+     0x100004e58 <+40>: movq   $0x0, 0x10(%rax) // 赋值 num 0
+     0x100004e60 <+48>: movq   %rax, %rdi
+     0x100004e63 <+51>: movq   %rax, -0x8(%rbp)
+     0x100004e67 <+55>: movq   %rdx, -0x10(%rbp)
+     0x100004e6b <+59>: callq  0x10000792e               ; symbol stub for: swift_retain
+     0x100004e70 <+64>: movq   -0x8(%rbp), %rdi
+     0x100004e74 <+68>: movq   %rax, -0x18(%rbp)
+     0x100004e78 <+72>: callq  0x100007928               ; symbol stub for: swift_release
+     0x100004e7d <+77>: movq   -0x10(%rbp), %rax
+     0x100004e81 <+81>: leaq   0x29e8(%rip), %rax        ; partial apply forwarder for plus #1 (Swift.Int) -> Swift.Int in getFn #1 () -> (Swift.Int) -> Swift.Int in _7_闭包 at <compiler-generated>
+     0x100004e88 <+88>: movq   -0x8(%rbp), %rdx
+     0x100004e8c <+92>: addq   $0x20, %rsp
+     0x100004e90 <+96>: popq   %rbp
+     0x100004e91 <+97>: retq
+     */
     typealias Fn = (Int) -> Int
     func getFn() -> Fn {
         var num = 0
@@ -123,6 +150,11 @@ do {
         }
         return plus
     }
+    var fn = getFn()
+    print(fn(1))
+    print(fn(2))
+    print(fn(3))
+    print(fn(4))
 }
 
 do {
@@ -134,14 +166,51 @@ do {
             return num
         }
     }
-    
+    /*
+     (lldb) register read rax
+     rax = 0x00000001006000a0
+     */
     var fn1 = getFn()
+    /*
+     (lldb) register read rax
+     rax = 0x00000001005009c0
+     */
     var fn2 = getFn()
+    /*
+     (lldb) x/4xg 0x00000001006000a0
+     0x1006000a0: 0x0000000100008100 0x0000000200000002
+     0x1006000b0: 0x0000000000000001 0x00027fff7016fd38
+     */
     print(fn1(1))
+    /*
+     (lldb) x/4xg 0x00000001005009c0
+     0x1005009c0: 0x0000000100008100 0x0000000200000002
+     0x1005009d0: 0x0000000000000002 0x0000000000000000
+     */
     print(fn2(2))
+    /*
+     (lldb) x/4xg 0x00000001006000a0
+     0x1006000a0: 0x0000000100008100 0x0000000200000002
+     0x1006000b0: 0x0000000000000004 0x00027fff7016fd38
+     */
     print(fn1(3))
+    /*
+     (lldb) x/4xg 0x00000001005009c0
+     0x1005009c0: 0x0000000100008100 0x0000000200000002
+     0x1005009d0: 0x0000000000000006 0x0000000000000000
+     */
     print(fn2(4))
+    /*
+     (lldb) x/4xg 0x00000001006000a0
+     0x1006000a0: 0x0000000100008100 0x0000000200000002
+     0x1006000b0: 0x0000000000000009 0x00027fff7016fd38
+     */
     print(fn1(5))
+    /*
+     (lldb) x/4xg 0x00000001005009c0
+     0x1005009c0: 0x0000000100008100 0x0000000200000002
+     0x1005009d0: 0x000000000000000c 0x0000000000000000
+     */
     print(fn2(6))
 }
 
@@ -270,10 +339,10 @@ do {
 
 do {
     // 全局作用域构成函数重载, 局部作用域不构成函数重载
-//    func getFirstPositive(_ v1: Int, _ v2: () -> Int) -> Int? {
-//        return v1 > 0 ? v1 : v2()
-//    }
-//    print(getFirstPositive(-4){ 20 })
+    //    func getFirstPositive(_ v1: Int, _ v2: () -> Int) -> Int? {
+    //        return v1 > 0 ? v1 : v2()
+    //    }
+    //    print(getFirstPositive(-4){ 20 })
     
     func getFirstPositive(_ v1: Int, _ v2: @autoclosure () -> Int) -> Int? {
         return v1 > 0 ? v1 : v2()
