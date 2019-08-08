6b066c31b0090dfd18b77962048c1523a35df54d
coderZsq <a13701777868@yahoo.com>
2019-08-08 13:15:43 +0800

    add

--- StudyNotes/Lang Collection/Swift/06-结构体和类/06-结构体和类/main.swift
+++ StudyNotes/Lang Collection/Swift/06-结构体和类/06-结构体和类/main.swift
@@ -88,14 +88,48 @@ do {
 // MARK: -  窥探初始化器的本质
 
 do {
+    /*
+     06-结构体和类`init() in Point #7 in :
+     ->  0x100002990 <+0>:  pushq  %rbp
+     0x100002991 <+1>:  movq   %rsp, %rbp
+     0x100002994 <+4>:  xorps  %xmm0, %xmm0
+     0x100002997 <+7>:  movaps %xmm0, -0x10(%rbp)
+     0x10000299b <+11>: movq   $0x0, -0x10(%rbp)
+     0x1000029a3 <+19>: movq   $0x0, -0x8(%rbp)
+     0x1000029ab <+27>: xorl   %eax, %eax
+     0x1000029ad <+29>: movl   %eax, %ecx
+     0x1000029af <+31>: movq   %rcx, %rax
+     0x1000029b2 <+34>: movq   %rcx, %rdx
+     0x1000029b5 <+37>: popq   %rbp
+     0x1000029b6 <+38>: retq
+     */
     struct Point {
         var x: Int = 0
         var y: Int = 0
     }
+    /*
+     ->  0x10000168d <+1773>: callq  0x100002990               ; init() -> Point #7 in _6_结构体和类 in Point #7 in _6_结构体和类 at main.swift:91
+     */
     var p = Point()
 }
 
 do {
+    /*
+     06-结构体和类`init() in Point #8 in :
+     ->  0x1000029c0 <+0>:  pushq  %rbp
+     0x1000029c1 <+1>:  movq   %rsp, %rbp
+     0x1000029c4 <+4>:  xorps  %xmm0, %xmm0
+     0x1000029c7 <+7>:  movaps %xmm0, -0x10(%rbp)
+     0x1000029cb <+11>: movq   $0x0, -0x10(%rbp)
+     0x1000029d3 <+19>: movq   $0x0, -0x8(%rbp)
+     0x1000029db <+27>: xorl   %eax, %eax
+     0x1000029dd <+29>: movl   %eax, %ecx
+     0x1000029df <+31>: movq   %rcx, %rax
+     0x1000029e2 <+34>: movq   %rcx, %rdx
+     0x1000029e5 <+37>: popq   %rbp
+     0x1000029e6 <+38>: retq
+     
+     */
     struct Point {
         var x: Int
         var y: Int
@@ -104,7 +138,15 @@ do {
             y = 0
         }
     }
+    /*
+     ->  0x1000016a0 <+1792>: callq  0x1000029c0               ; init() -> Point #8 in _6_结构体和类 in Point #8 in _6_结构体和类 at main.swift:120
+     */
     var p = Point()
+    print(Mems.memStr(ofVal: &p))
+    
+    print(MemoryLayout<Point>.size) // 16
+    print(MemoryLayout<Point>.stride) // 16
+    print(MemoryLayout<Point>.alignment) // 8
 }
 
 // MARK - 结构体内存结构
@@ -115,6 +157,9 @@ do {
         var y: Int = 0
         var origin: Bool = false
     }
+    var p = Point()
+    print(Mems.memStr(ofVal: &p))
+    
     print(MemoryLayout<Point>.size) // 17
     print(MemoryLayout<Point>.stride) // 24
     print(MemoryLayout<Point>.alignment) // 8
@@ -165,6 +210,8 @@ do {
 
 // MARK: - 结构体与类的本质区别
 
+import Foundation
+
 do {
     class Size {
         var width = 1
@@ -176,23 +223,78 @@ do {
         var y = 4
     }
     
+    var size = Size()
+    print(malloc_size(Mems.ptr(ofRef: size)))
+    
+    //    var ptr = malloc(16)
+    //    print(malloc_size(ptr))
+    
+    print("MemoryLayout<Size>.stride", MemoryLayout<Size>.stride)
+    print("MemoryLayout<Point>.stride", MemoryLayout<Point>.stride)
+    
     func test() {
+        /*
+         0x100002d3d <+29>: callq  0x100007c40               ; type metadata accessor for Size #1 in _6_结构体和类 at <compiler-generated>
+         0x100002d42 <+34>: movq   %rax, %r13
+         0x100002d45 <+37>: movq   %rdx, -0x28(%rbp)
+         0x100002d49 <+41>: callq  0x100002d70               ; __allocating_init() -> Size #1 in _6_结构体和类 in Size #1 in _6_结构体和类 at main.swift:214
+         */
+        /*
+         0x100002d9f <+47>: callq  0x10000d7d2               ; symbol stub for: swift_allocObject
+         0x7fff647fabdf <+15>: callq  0x7fff647fab50            ; swift_slowAlloc
+         0x7fff647fab64 <+20>: callq  0x7fff648736d2            ; symbol stub for: malloc
+         */
         var size = Size()
+        print(Mems.size(ofRef: size))
+        print("size变量的地址", Mems.ptr(ofVal: &size))
+        print("size变量的内存", Mems.memStr(ofVal: &size))
+        print("size所指向内存的地址", Mems.ptr(ofRef: size))
+        print("size所指向内存的内容", Mems.memStr(ofRef: size))
+        
         var point = Point()
+        print("point变量的地址", Mems.ptr(ofVal: &point))
+        print("point变量的内存", Mems.memStr(ofVal: &point))
     }
+    test()
 }
 
 // MARK: - 值类型
 
 do {
+    /*
+     06-结构体和类`init(x:y:) in Point #15 in :
+     0x100004040 <+0>:  pushq  %rbp
+     0x100004041 <+1>:  movq   %rsp, %rbp
+     0x100004044 <+4>:  movq   %rdi, %rax
+     0x100004047 <+7>:  movq   %rsi, %rdx
+     0x10000404a <+10>: popq   %rbp
+     0x10000404b <+11>: retq
+     */
     struct Point {
         var x: Int
         var y: Int
     }
     
     func test() {
+        /*
+         0x100003d43 <+19>:  movl   $0xa, %edi
+         0x100003d48 <+24>:  movl   $0x14, %esi
+         0x100003d4d <+29>:  callq  0x100004040               ; init(x: Swift.Int, y: Swift.Int) -> Point #15 in _6_结构体和类 in Point #15 in _6_结构体和类 at main.swift:264
+         */
+        /*
+         0x100003d52 <+34>:  movq   %rax, -0x10(%rbp)
+         0x100003d56 <+38>:  movq   %rdx, -0x8(%rbp)
+         */
         var p1 = Point(x: 10, y: 20)
+        /*
+         0x100003d5a <+42>:  movq   %rax, -0x20(%rbp)
+         0x100003d5e <+46>:  movq   %rdx, -0x18(%rbp)
+         */
         var p2 = p1
+        /*
+         0x100003d62 <+50>:  movq   $0xb, -0x20(%rbp)
+         0x100003d6a <+58>:  movq   $0x16, -0x18(%rbp)
+         */
         p2.x = 11
         p2.y = 22
         print(p1.x, p1.y)
@@ -250,9 +352,33 @@ do {
     }
     
     func test() {
+        /*
+         (lldb) register read rax
+         rax = 0x000000010053aaa0
+         */
+        /*
+         30 2E 01 00 01 00 00 00
+         02 00 00 00 00 00 00 00
+         0A 00 00 00 00 00 00 00
+         14 00 00 00 00 00 00 00
+         */
+        /*
+         (lldb) register read rbp
+         rbp = 0x00007ffeefbfebd0
+         */
+        // 0x1000040a6 <+70>:  movq   %rax, -0x10(%rbp)
         var s1 = Size(width: 10, height: 20)
+        // 0x1000040ad <+77>:  movq   %rax, -0x90(%rbp)
         var s2 = s1
+        /*
+         0x100004109 <+169>: movq   -0x90(%rbp), %rax
+         0x100004110 <+176>: movq   $0xb, 0x10(%rax)
+         */
         s2.width = 11
+        /*
+         0x10000415a <+250>: movq   -0x90(%rbp), %rax
+         0x100004161 <+257>: movq   $0x16, 0x18(%rax)
+         */
         s2.height = 22
         print(s1.width, s1.height)
     }
@@ -305,7 +431,7 @@ do {
             self.height = height
         }
     }
-
+    
     let p = Point(x: 10, y: 20)
     
     let s = Size(width: 10, height: 20)
