d064d75b9907f72dc44e748c576abdaed1c6cca9
coderZsq <a13701777868@yahoo.com>
2020-12-04 13:27:43 +0800

    add

---
+++ StudyNotes/Lang Collection/Objective-C/15-数据结构与算法/15-数据结构与算法/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  15-数据结构与算法
+//
+//  Created by 朱双泉 on 2020/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
