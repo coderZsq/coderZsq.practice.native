a390c65814fbf29673d193ac4c4dcac0937ca669
coderZsq <a13701777868@yahoo.com>
2021-01-20 12:14:02 +0800

    add

---
+++ StudyNotes/iOS Collection/App/75 | 使用URL Scheme进行App间的唤起和通信/TestScheme/TestScheme/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestScheme
+//
+//  Created by 朱双泉 on 2021/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ StudyNotes/iOS Collection/App/75 | 使用URL Scheme进行App间的唤起和通信/TestScheme/TestScheme/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestScheme
+//
+//  Created by 朱双泉 on 2021/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
