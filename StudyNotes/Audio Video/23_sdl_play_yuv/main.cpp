4a9ac3db9decef1225d4eac9ae58c856a46c8ffc
coderZsq <a13701777868@yahoo.com>
2021-05-11 16:46:01 +0800

    add

---
+++ StudyNotes/Audio Video/23_sdl_play_yuv/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#undef main
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
