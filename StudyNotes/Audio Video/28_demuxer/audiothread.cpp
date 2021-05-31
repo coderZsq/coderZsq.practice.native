8a21e086927c10a7172a07789313944ec851d4a1
coderZsq <a13701777868@yahoo.com>
2021-05-31 15:49:52 +0800

    add

---
+++ StudyNotes/Audio Video/28_demuxer/audiothread.cpp
@@ -0,0 +1,42 @@
+#include "audiothread.h"
+
+#include <QDebug>
+#include "demuxer.h"
+
+extern "C" {
+#include <libavutil/imgutils.h>
+}
+
+AudioThread::AudioThread(QObject *parent) : QThread(parent) {
+    // 当监听到线程结束时（finished），就调用deleteLater回收内存
+    connect(this, &AudioThread::finished,
+            this, &AudioThread::deleteLater);
+}
+
+AudioThread::~AudioThread() {
+    // 断开所有的连接
+    disconnect();
+    // 内存回收之前，正常结束线程
+    requestInterruption();
+    // 安全退出
+    quit();
+    wait();
+    qDebug() << this << "析构（内存被回收）";
+}
+
+void AudioThread::run() {
+    AudioDecodeSpec aOut;
+    aOut.filename = "/Users/zhushuangquan/Desktop/out.pcm";
+
+    VideoDecodeSpec vOut;
+    vOut.filename = "/Users/zhushuangquan/Desktop/out.yuv";
+
+    Demuxer().demux("/Users/zhushuangquan/Desktop/in.mp4", aOut, vOut);
+
+    qDebug() << aOut.sampleRate
+             << av_get_channel_layout_nb_channels(aOut.chLayout)
+             << av_get_sample_fmt_name(aOut.sampleFmt);
+
+    qDebug() << vOut.width << vOut.height
+             << vOut.fps << av_get_pix_fmt_name(vOut.pixFmt);
+}
