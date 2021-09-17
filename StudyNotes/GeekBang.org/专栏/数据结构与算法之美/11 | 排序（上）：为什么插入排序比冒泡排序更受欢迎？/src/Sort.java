21c0791c1c3c6f8dbbafc8ea183de7c167388bef
coderZsq <a13701777868@yahoo.com>
2021-09-17 14:16:42 +0800

    add

---
+++ StudyNotes/GeekBang.org/专栏/数据结构与算法之美/11 | 排序（上）：为什么插入排序比冒泡排序更受欢迎？/src/Sort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Sort {
+
+    public static void main(String[] args) {
+
+        int[] a = {5, 3, 2, 1, 4, 5, 1, 2};
+        bubbleSort(a, a.length);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void bubbleSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            boolean flag = false;
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    flag = true;
+                }
+            }
+            if (!flag) break;
+        }
+    }
+
+    public static void insertionSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            int value = a[i];
+
+        }
+    }
+}
