a85cc8c9eb80b460fd851f65f854d498101131f4
coderZsq <a13701777868@yahoo.com>
2021-09-18 22:04:49 +0800

    add

--- StudyNotes/GeekBang.org/专栏/数据结构与算法之美/16 | 二分查找（下）：如何快速定位IP对应的省份地址？/src/search/BSearch.java
+++ StudyNotes/GeekBang.org/专栏/数据结构与算法之美/16 | 二分查找（下）：如何快速定位IP对应的省份地址？/src/search/BSearch.java
@@ -3,13 +3,64 @@ package search;
 public class BSearch {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 6, 6, 7};
-        System.out.println(bsearch(a, a.length, 6));
-        System.out.println(bsearch2(a, a.length, 6));
-        System.out.println(bsearch3(a, a.length, 6));
+        int[] a = {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
+        System.out.println(bsearch(a, a.length, 5));
+        System.out.println(bsearch2(a, a.length, 5));
+        System.out.println(bsearch3(a, a.length, 8));
+        System.out.println(bsearch4(a, a.length, 8));
+        System.out.println(bsearch5(a, a.length, 8));
+        System.out.println(bsearch6(a, a.length, 7));
+        System.out.println(bsearch7(a, a.length, 7));
     }
 
-    public static int bsearch3(int[] a, int n, int value) {
+    public static int bsearch7(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int bsearch6(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int bsearch5(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == n - 1) || a[mid + 1] != value) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int bsearch4(int[] a, int n, int value) {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
@@ -24,6 +75,22 @@ public class BSearch {
             }
         }
         return -1;
+    }
+
+    public static int bsearch3(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (low < n && a[low] == value) return low;
+        else return -1;
     }
 
     public static int bsearch2(int[] a, int n, int val) {
