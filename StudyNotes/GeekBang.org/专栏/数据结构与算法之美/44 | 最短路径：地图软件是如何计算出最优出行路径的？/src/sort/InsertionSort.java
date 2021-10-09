ad530dfa4396c4c69ad18a54ba8ad76cac52f39b
coderZsq <a13701777868@yahoo.com>
2021-10-09 16:46:57 +0800

    add

---
+++ StudyNotes/GeekBang.org/专栏/数据结构与算法之美/44 | 最短路径：地图软件是如何计算出最优出行路径的？/src/sort/InsertionSort.java
@@ -0,0 +1,31 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] a = {5, 3, 2, 1, 4, 5, 1, 2};
+        insertionSort(a, a.length);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void insertionSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            int value = a[i];
+            int j = i - 1;
+
+            for (; j >= 0; --j) {
+                if (a[j] > value) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+
+            a[j + 1] = value;
+        }
+    }
+}
