49c0c349138c5001182f95e01a55e0ba2060e22d
coderZsq <a13701777868@yahoo.com>
2023-03-08 22:32:01 +0800

    [feat] How to use AsyncPublisher to convert @Published to Async / Await

---
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] Swift Concurrency/SwiftConcurrencyBootcamp/AsyncPublisherBootcamp.swift
@@ -0,0 +1,86 @@
+//
+//  AsyncPublisherBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by 朱双泉 on 2023/3/8.
+//
+
+import SwiftUI
+import Combine
+
+class AsyncPublisherDataManager {
+    @Published var myData: [String] = []
+    
+    func addData() async {
+        myData.append("Apple")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        myData.append("Banana")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        myData.append("Orange")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        myData.append("Watermelon")
+    }
+}
+
+class AsyncPublishBootcampViewModel: ObservableObject {
+    @MainActor @Published var dataArray: [String] = []
+    let manager = AsyncPublisherDataManager()
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        Task {
+            for await value in manager.$myData.values {
+                await MainActor.run(body: {
+                    self.dataArray =  value
+                })
+            }
+        }
+//        Task {
+//            for await value in manager.$myData.values {
+//                await MainActor.run(body: {
+//                    self.dataArray =  value
+//                })
+//            }
+//        } 
+        
+//        manager.$myData
+//            .receive(on: DispatchQueue.main, options: nil)
+//            .sink { dataArray in
+//                self.dataArray = dataArray
+//            }
+//            .store(in: &cancellables)
+    }
+    
+    func start() async {
+        await manager.addData()
+    }
+}
+
+struct AsyncPublisherBootcamp: View {
+    
+    @StateObject private var viewModel = AsyncPublishBootcampViewModel()
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(viewModel.dataArray, id: \.self) {
+                    Text($0)
+                        .font(.headline)
+                }
+            }
+        }
+        .task {
+            await viewModel.start()
+        }
+    }
+}
+
+struct AsyncPublisherBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncPublisherBootcamp()
+    }
+}
