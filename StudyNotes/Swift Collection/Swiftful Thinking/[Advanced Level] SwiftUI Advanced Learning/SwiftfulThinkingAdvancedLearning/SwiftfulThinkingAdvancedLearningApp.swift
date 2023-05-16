9900a47d692664069e798ed68478194682c9a9fc
coderZsq <a13701777868@yahoo.com>
2023-05-16 10:35:16 +0800

    feat: How to create custom Transitions in SwiftUI

--- StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftfulThinkingAdvancedLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ViewModifierBootcamp()
+            AnyTransitionBootcamp()
         }
     }
 }
