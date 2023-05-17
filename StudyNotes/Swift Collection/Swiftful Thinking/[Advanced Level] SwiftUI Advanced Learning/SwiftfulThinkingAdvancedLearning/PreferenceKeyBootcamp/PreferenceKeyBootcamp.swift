be8f921feb1752c2b2592ac0375fd5daa4f9c9ff
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-05-17 11:29:11 +0800

    feat: Use PreferenceKey to extract values from child views in SwiftUI

---
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/PreferenceKeyBootcamp/PreferenceKeyBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  PreferenceKeyBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by 朱双泉 on 2023/5/17.
+//
+
+import SwiftUI
+
+struct PreferenceKeyBootcamp: View {
+    
+    @State private var text: String = "Hello, world!"
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                SecondaryScreen(text: text)
+                    .navigationTitle("Navigation Title")
+            }
+        }
+        .onPreferenceChange(CustomTitlePreferenceKey.self, perform: { value in
+            self.text = value
+        })
+    }
+}
+
+extension View {
+    
+    func customTitle(_ text: String) -> some View {
+        preference(key: CustomTitlePreferenceKey.self, value: text)
+    }
+     
+}
+
+struct PreferenceKeyBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceKeyBootcamp()
+    }
+}
+
+struct SecondaryScreen: View {
+    
+    let text: String
+    @State private var newValue: String = ""
+    
+    var body: some View {
+        Text(text)
+            .onAppear(perform: getDataFromDatabase)
+            .customTitle(newValue)
+    }
+    
+    func getDataFromDatabase() {
+        // download
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.newValue = "NEW VALUE FROM DATABASE"
+        }
+    }
+}
+
+struct CustomTitlePreferenceKey: PreferenceKey {
+    
+    static var defaultValue: String = ""
+    
+    static func reduce(value: inout String, nextValue: () -> String) {
+        value = nextValue()
+    }
+}
