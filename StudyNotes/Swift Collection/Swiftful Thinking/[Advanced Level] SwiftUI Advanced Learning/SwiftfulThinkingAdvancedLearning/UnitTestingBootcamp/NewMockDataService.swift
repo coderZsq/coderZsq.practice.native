881146edbac4733b0ebf625b1478336ad08d6c79
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-05-25 00:03:10 +0800

    feat: Unit Testing a SwiftUI application in Xcode

---
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/UnitTestingBootcamp/NewMockDataService.swift
@@ -0,0 +1,43 @@
+//
+//  NewMockDataService.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by 朱双泉 on 2023/5/24.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+protocol NewDataServiceProtocol {
+    func downloadItemsWithEscaping(completion: @escaping (_ items: [String]) -> ())
+    func downloadItemsWithCombine() -> AnyPublisher<[String], Error>
+}
+
+class NewMockDataService: NewDataServiceProtocol {
+    
+    let items: [String]
+    
+    init(items: [String]?) {
+        self.items = items ?? [
+            "ONE", "TWO", "THREE"
+        ]
+    }
+    
+    func downloadItemsWithEscaping(completion: @escaping (_ items: [String]) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            completion(self.items)
+        }
+    }
+    
+    func downloadItemsWithCombine() -> AnyPublisher<[String], Error> {
+        Just(items)
+            .tryMap({ publishedItems in
+                guard !publishedItems.isEmpty else {
+                    throw URLError(.badServerResponse)
+                }
+                return publishedItems
+            })
+            .eraseToAnyPublisher()
+    }
+}
