1a87789c6170a66dfe223bc928b7edb8e8e12f6d
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-05-26 22:11:31 +0800

    feat: UI Testing a SwiftUI application in Xcode

--- StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -2,17 +2,28 @@
 //  SwiftfulThinkingAdvancedLearningApp.swift
 //  SwiftfulThinkingAdvancedLearning
 //
-//  Created by Farben on 2023/3/15.
+//  Created by 朱双泉 on 2023/3/15.
 //
 
 import SwiftUI
 
 @main
 struct SwiftfulThinkingAdvancedLearningApp: App {
+    
+    let currentUserIsSignedIn: Bool
+    
+    init() {
+        //let userIsSignedIn: Bool = CommandLine.arguments.contains("-UITest_startSignedIn") ? true : false
+          let userIsSignedIn: Bool = ProcessInfo.processInfo.arguments.contains("-UITest_startSignedIn") ? true : false
+        //let value = ProcessInfo.processInfo.environment["-UITest_startSignedIn2"]
+        //let userIsSignedIn: Bool = value == "true" ? true : false
+        self.currentUserIsSignedIn = userIsSignedIn
+    }
+    
     var body: some Scene {
         WindowGroup {
-            AppTabBarView()
-//            MatchedGeometryEffectExample2()
+            UITestingBootcampView(currentUserIsSignedIn: currentUserIsSignedIn )
         }
     }
 }
+ 
