8cf180a80c3a39830a53fddebe13852ee89485db
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-06-19 20:32:23 +0800

    feat: Send Push Notifications using CloudKit in SwiftUI

--- StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Advanced Level] SwiftUI Advanced Learning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -22,7 +22,8 @@ struct SwiftfulThinkingAdvancedLearningApp: App {
     
     var body: some Scene {
         WindowGroup {
-            CloudKitCrudBootcamp()
+//            CloudKitPushNotificationBootcamp()
+            CloudKitCrudBootcamp() 
 //            CloudKitUserBootcamp()
 //            UITestingBootcampView(currentUserIsSignedIn: currentUserIsSignedIn )
         }
