6f17146edfae9b038f2aa6212241ccbd91d9be8a
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-07-05 00:00:03 +0800

    feat: iOS Firebase Authentication: Sign In With Email & Password Tutorial (2/2)

--- StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/AuthenticationMananger.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/AuthenticationMananger.swift
@@ -33,9 +33,36 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: user)
     }
     
+    @discardableResult
     func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
         return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }
+    
+    func updatePassword(password: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        try await user.updatePassword(to: password)
+    }
+    
+    func updateEmail(email: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        try await user.updateEmail(to: email)
     }
     
     func signOut() throws {
--- StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/RootView.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/RootView.swift
@@ -23,7 +23,7 @@ struct RootView: View {
         }
         .fullScreenCover(isPresented: $showSignInView) {
             NavigationStack {
-                AuthenticationView()
+                AuthenticationView(showSignInView: $showSignInView)
             }
         }
     }
