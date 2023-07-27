44cdea4642dd68d686181fa6026475070d8f3ad7
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-07-27 20:23:37 +0800

    feat: iOS Firebase Authentication: Deleting Users

--- StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/SettingsView.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Authentication/SettingsView.swift
@@ -27,6 +27,10 @@ final class SettingsViewModel: ObservableObject {
         try AuthenticationManager.shared.signOut()
     }
     
+    func deleteAccount() async throws {
+        try await AuthenticationManager.shared.delete()
+    }
+    
     func resetPassword() async throws {
         let authUser = try AuthenticationManager.shared.getAuthenticatedUser()
         
@@ -83,6 +87,20 @@ struct SettingsView: View {
                     }
                 }
             }
+            
+            Button(role: .destructive) {
+                Task {
+                    do {
+                        try await viewModel.deleteAccount()
+                        showSignInView = true
+                    } catch {
+                        print(error)
+                    }
+                }
+            } label: {
+                Text("Delete account")
+            }
+
             
             if viewModel.authProviders.contains(.email) {
                 emailSection
