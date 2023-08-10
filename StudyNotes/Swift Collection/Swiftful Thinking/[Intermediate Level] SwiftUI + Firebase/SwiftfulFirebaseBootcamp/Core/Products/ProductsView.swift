62eb188149e9f22364fa0e1dfb629326441917df
zhushuanguquan <shuangquan.zhu@volvocars.com>
2023-08-10 14:40:02 +0800

    feat: Firebase Firestore: Sorting, Filtering, and Querying in iOS

--- StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Core/Products/ProductsView.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Core/Products/ProductsView.swift
@@ -11,9 +11,71 @@ import SwiftUI
 final class ProductsViewModel: ObservableObject {
     
     @Published private(set) var products: [Product] = []
+    @Published var selectedFilter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.shared.getAllProducts()
+//    func getAllProducts() async throws {
+//        self.products = try await ProductsManager.shared.getAllProducts()
+//    }
+    
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: return nil
+            case .priceHigh: return true
+            case .priceLow: return false
+            }
+        }
+    }
+    
+    func filterSelected(option: FilterOption) async throws {
+        self.selectedFilter = option
+        self.getProducts()
+//        switch option {
+//        case .noFilter:
+//            self.products = try await ProductsManager.shared.getAllProducts()
+//        case .priceHigh:
+//            self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: true)
+//            break
+//        case .priceLow:
+//            self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: false)
+//            break
+//        }
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case smartphones
+        case laptops
+        case fragrances
+        
+        var categoryKey: String? {
+            if self == .noCategory {
+                return nil
+            }
+            return self.rawValue
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        self.selectedCategory = option
+        self.getProducts()
+//        switch option {
+//        case .noCategory:
+//            self.products = try await ProductsManager.shared.getAllProducts()
+//        case .smartphones, .laptops, .fragrances:
+//            self.products = try await ProductsManager.shared.getAllProductsForCategory(category: option.rawValue)
+//        }
+    }
+    
+    func getProducts() {
+        Task {
+            self.products = try await ProductsManager.shared.getAllProducts(priceDescending: selectedFilter?.priceDescending, forCategory: selectedCategory?.categoryKey)
+        }
     }
 }
 
@@ -28,8 +90,33 @@ struct ProductsView: View {
             }
         }
         .navigationTitle("Products")
-        .task {
-            try? await viewModel.getAllProducts()
+        .toolbar(content: {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Menu("Filter: \(viewModel.selectedFilter?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.filterSelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Menu("Filter: \(viewModel.selectedCategory?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.categorySelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .onAppear {
+            viewModel.getProducts()
         }
     }
 }
--- StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Firestore/ProductsManager.swift
+++ StudyNotes/Swift Collection/Swiftful Thinking/[Intermediate Level] SwiftUI + Firebase/SwiftfulFirebaseBootcamp/Firestore/ProductsManager.swift
@@ -28,8 +28,40 @@ final class ProductsManager {
         try await productDocument(productId: productId).getDocument(as: Product.self)
     }
     
-    func getAllProducts() async throws -> [Product] {
-        try await productsCollection.getDocuments(as: Product.self)
+    private func getAllProducts() async throws -> [Product] {
+        try await productsCollection
+            .getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsSortedByPrice(descending: Bool) async throws -> [Product] {
+        try await productsCollection
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsForCategory(category: String) async throws -> [Product] {
+        try await productsCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .getDocuments(as: Product.self)
+    }
+    
+    private func getAllProductsByPriceAndCategory(descending: Bool, category: String) async throws -> [Product] {
+        try await productsCollection
+            .whereField(Product.CodingKeys.category.rawValue, isEqualTo: category)
+            .order(by: Product.CodingKeys.price.rawValue, descending: descending)
+            .getDocuments(as: Product.self)
+    }
+    
+    func getAllProducts(priceDescending descending: Bool?, forCategory category: String?) async throws -> [Product] {
+        if let descending, let category {
+            return try await getAllProductsByPriceAndCategory(descending: descending, category: category)
+        } else if let descending {
+            return try await getAllProductsSortedByPrice(descending: descending)
+        } else if let category {
+            return try await getAllProductsForCategory(category: category)
+        }
+        
+        return try await getAllProducts()
     }
 }
 
